90be70a9f43ca157603c264dd8ede4aad97796ff
Nach0z <wtechboy18@gmail.com>
2012-03-14 14:12:05 -0400

    code revision.

--- src/main/java/org/nach0z/mineestate/MineEstatePlugin.java
+++ src/main/java/org/nach0z/mineestate/MineEstatePlugin.java
@@ -12,10 +12,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.milkbowl.vault.Vault;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.permission.Permission;
 import java.util.*;
 import java.io.*;
 
 public class MineEstatePlugin extends JavaPlugin implements Listener{
+    private Economy econ = null;
     private YAMLProcessor wg_config;
     private WorldGuardPlugin worldguard_plugin;
     private ArrayList<String> available;
@@ -39,8 +45,12 @@ public class MineEstatePlugin extends JavaPlugin implements Listener{
 	}
 	else
 		System.out.println("Not a WorldGuardPlugin!");
-	System.out.println(worldguard_plugin.getDataFolder());
-	System.out.println(WORLDGUARD.getDataFolder());
+	if(!setupEcon()) {
+		System.out.println("[FATAL ERROR] MineEdit requires Vault to be installed to interface with the economy! Please install Vault");
+		getServer().getPluginManager().disablePlugin(this);
+		return;
+
+	}
 /*	if(!WORLDGUARD.getConfig().getBoolean("regions.sql.use")) {
 		System.out.println("[FATAL ERROR] MineEstates REQUIRES WorldGuard to be using the MySQL Database for functionality!");
 		return;
@@ -56,11 +66,13 @@ public class MineEstatePlugin extends JavaPlugin implements Listener{
 	} catch (Exception e) {
 		System.out.println(e);
 		System.out.println("[FATAL ERROR] MineEstates unable to read WorldGuard config file! Please install WorldGuard before using MineEstates.");
+		getServer().getPluginManager().disablePlugin(this);
 		return;
 	}
 
 	if (!wg_config.getBoolean("regions.sql.use")) {
 		System.out.println("[FATAL ERROR] MineEstates requires WorldGuard to be using the MySQL Database method currently. Later releases may allow non-MySQL databases.");
+		getServer().getPluginManager().disablePlugin(this);
 		return;
 	 } else {
 
@@ -70,6 +82,18 @@ public class MineEstatePlugin extends JavaPlugin implements Listener{
 		db = new MySqlConnector(this, wg_config);
 		available = db.getAvailable();
 	}
+    }
+
+    public boolean setupEcon() {
+	if(getServer().getPluginManager().getPlugin("Vault") == null)
+		return false;
+	RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+	if (rsp == null)
+		return false;
+	econ = rsp.getProvider();
+		return econ != null;
+
+
     }
 
 }
