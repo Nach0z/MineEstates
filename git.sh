1db265380683d51ef48abf65c78d9f333cd97b2e
Nach0z <wtechboy18@gmail.com>
2012-08-28 13:02:13 -0400

    Generic code revision

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 find . -path './.git' -prune -o -type f -exec git add '{}' \;
-if [ -z "#@" ]
+if [ -z "$@" ]
 then
 	git commit -m 'Generic code revision'
 else
-	git commit -m '$@'
+	git commit -m "$@"
 fi
 
 git push -u origin master
