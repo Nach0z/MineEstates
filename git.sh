6aa8f94eca6fe434014bfba77fbc8371cb712db6
Nach0z <wtechboy18@gmail.com>
2012-08-28 13:01:35 -0400

    $@

--- git.sh
+++ git.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 find . -path './.git' -prune -o -type f -exec git add '{}' \;
-git commit -m 'code revision.'
+if [ -z "#@" ]
+then
+	git commit -m 'Generic code revision'
+else
+	git commit -m '$@'
+fi
+
 git push -u origin master
